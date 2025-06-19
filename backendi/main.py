493bbde4bc4cba3af53b63fac6cee1cfa38a71aa
Amirasophie <amira.sophie.repenning@gmail.com>
2025-06-19 13:57:00 +0200

    Restrict CORS origins to specific domain

--- backendi/main.py
+++ backendi/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # restrict in production
+    allow_origins=["https://mussels.yperion.dev"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
